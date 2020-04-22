264fa1142d44defcfb2a9a6905c07fad9faa6606
faatihyucel <faatihyucel@gmail.com>
2020-04-21 22:00:15 -0700

    hello git!

--- src/day01_FirstProgramming/HelloWorld.java
+++ src/day01_FirstProgramming/HelloWorld.java
@@ -5,10 +5,7 @@ public class HelloWorld  {
     public static void main( String [] args ) {
 
         System.out.println("Hello Word");
-        System.out.println("Hello Word");
-        System.out.println("Hello Word");
-        System.out.println("Hello Word");
-        System.out.println("Hello Word");
+
 
 
         /*
