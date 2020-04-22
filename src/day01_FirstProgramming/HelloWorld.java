a66b61722f290e5b8fdf01f96d08e23524eb76c6
faatihyucel <faatihyucel@gmail.com>
2020-04-21 21:51:38 -0700

    second commit

--- src/day01_FirstProgramming/HelloWorld.java
+++ src/day01_FirstProgramming/HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld  {
     public static void main( String [] args ) {
 
         System.out.println("Hello Word");
+        System.out.println("Hello Word");
+        System.out.println("Hello Word");
+        System.out.println("Hello Word");
+        System.out.println("Hello Word");
+
 
         /*
         date : 03/09/2020
